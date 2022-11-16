5b79a03cca045417711e8ff5b2f17c273474c70f
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-11-16 17:07:33 +0200

    spa was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const pages = document.querySelectorAll('.page__plan');
+const links = document.querySelectorAll('.about__link');
+
+pages[1].hidden = true;
+
+const handleClick = (event, currentLink) => {
+  event.preventDefault();
+
+  links.forEach(link => {
+    if (currentLink.href !== window.document.URL) {
+      link.classList.toggle('about__link--active');
+    }
+  });
+
+  window.history.pushState({}, '', event.target.href);
+
+  pages.forEach(page => {
+    const match = page.dataset.match;
+
+    page.hidden = window.location.pathname !== match;
+  });
+};
+
+const createLinks = () => {
+  links.forEach(link => {
+    link.addEventListener('click', (event) => handleClick(event, link));
+  });
+};
+
+createLinks();
